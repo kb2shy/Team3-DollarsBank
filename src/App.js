190fc7ecd9edaff77dfa94e82ae486ff99f03af9
Chhaian Pin <chhaianpin@hotmail.com>
2020-08-31 15:40:46 -0700

    created Landing component with background image.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import MenuBar from './MenuBar';
+import Landing from './Landing';
 
 function App() {
   return (
     <div className="App">
       <MenuBar />
+      <Landing />
     </div>
   );
 }
